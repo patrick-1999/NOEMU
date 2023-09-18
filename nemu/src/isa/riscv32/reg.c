6bff2069351b77216df622078eaf6b150f825666
tracer-ics2023 <tracer@njuics.org>
2023-09-18 14:51:36 +0800

    >  compile NEMU 222050208 PXH Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:51:36 up 1 day, 15:06,  1 user,  load average: 3.05, 2.01, 1.60

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -38,5 +38,12 @@ void isa_reg_display() {\
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  // for(int i=0;i<32;i++){
+  //   bool result = strcmp(s,regs[i]);
+  //   if (result == true){
+  //     return cpu.reg[i];
+  //     }
+
+  // }
   return 0;
 }
