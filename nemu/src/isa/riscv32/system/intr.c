d96ba98293a71b7745f8df1dfe859f6ded2f677b
tracer-ics2023 <tracer@njuics.org>
2023-10-07 18:54:51 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:54:51 up 5 days, 17:42,  1 user,  load average: 0.78, 0.37, 0.20

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -20,6 +20,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+    printf("isa_raise_intr-NO:%x",NO);
     cpu.csr.mcause = NO;
     cpu.csr.mepc = epc;
   return cpu.csr.mtvec;
