0c620ca017edc5824516f6953bff2a2cf3da3dde
tracer-ics2023 <tracer@njuics.org>
2023-10-07 14:40:56 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:40:56 up 5 days, 13:28,  1 user,  load average: 0.05, 0.13, 0.14

--- abstract-machine/scripts/riscv32-nemu.mk
+++ abstract-machine/scripts/riscv32-nemu.mk
@@ -2,7 +2,7 @@ include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
 # COMMON_CFLAGS += -march=rv32im_zicsr -mabi=ilp32  # overwrite
-COMMON_CFLAGS += -march=rv32g -mabi=ilp32d  # overwrite
+COMMON_CFLAGS += -march=rv32g -mabi=ilp32  # overwrite
 LDFLAGS       += -melf32lriscv                     # overwrite
 
 AM_SRCS += riscv/nemu/start.S \
