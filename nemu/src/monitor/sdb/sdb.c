ea01ddcc1103ad7ef2560e9329ddd0efc84805c8
tracer-ics2023 <tracer@njuics.org>
2023-10-08 10:44:07 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:44:07 up  1:38,  1 user,  load average: 0.25, 0.14, 0.13

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -105,7 +105,8 @@ static int cmd_w(char *args) {
   // new->args = str;
   printf("get node\n");
   word_t res = expr(args, &success);
-  char str[20]="test";
+  char str[20]="default";
+  strcpy(str,args);
   new->args=str;
   new->val=res;
   printf("add watch point :NO.%d expression : %s, init_value = %d.\n", new->NO, new->args, new->val);
