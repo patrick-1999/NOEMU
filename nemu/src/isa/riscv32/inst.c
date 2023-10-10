b574b511bdcd45819c4e02b3127e384c7def88da
tracer-ics2023 <tracer@njuics.org>
2023-10-10 10:09:17 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:09:17 up 17:23,  1 user,  load average: 0.33, 0.26, 0.31

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -204,6 +204,7 @@ word_t jump_jal(int32_t imm, Decode *s, int dest) {
 word_t jump_jalr(int32_t imm, Decode *s, uint32_t src1, int dest) {
   // s->dnpc = (2 * imm + src1) & (~1);
   s->dnpc = (src1 + imm) & (~1);
+  
   #ifdef CONFIG_FTRACE_COND
   int is_ret = -1;
     char *func_name = get_func_name(s->snpc - 4, s->dnpc, &is_ret);
