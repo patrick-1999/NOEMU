849c885d4e87e8aba1437cae706ad75b546c7b7a
tracer-ics2023 <tracer@njuics.org>
2023-10-11 14:33:05 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:33:05 up  6:09,  1 user,  load average: 1.06, 0.40, 0.24

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,10 +8,26 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
-
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  // 现在的代码存储在ramdisk中，我们需要解析elf文件，将其需要执行的部分加载到内存中并且返回开始执行代码的入口地址
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  //通过魔数来判断是否是一个elf文件 check valid elf 
+  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    if (phdr[i].p_type == PT_LOAD) {
+      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      // 这句代码实现将代码程序读入到内存中，起始地址是p_vaddr,长度为p_memsz
+      // set .bss with zeros
+      memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+      // 因为对齐会在导致文件大小和内存中的大小不一致，内存中在最后会添加0补齐
+    }
+  }
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
