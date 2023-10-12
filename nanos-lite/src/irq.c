4943956bd01455ff6bfd480ebe5ff86c116bb36b
tracer-ics2023 <tracer@njuics.org>
2023-10-12 12:04:05 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:04:05 up  9:06,  1 user,  load average: 0.56, 0.26, 0.17

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,9 +2,9 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case 4 : 
-      printf("syscall");
-      break;
+    // case 4 : 
+    //   printf("syscall");
+    //   break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
