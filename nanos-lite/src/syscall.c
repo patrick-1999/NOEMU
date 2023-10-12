3043bdd1f57708074ea48af9da9c9b47d67057bb
tracer-ics2023 <tracer@njuics.org>
2023-10-12 16:15:18 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:15:18 up 13:17,  1 user,  load average: 0.14, 0.22, 0.24

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,6 @@
 #include <common.h>
 #include "syscall.h"
+
 void halt(int code);
 void write(uintptr_t *a){
   if(a[1]==1){
@@ -21,22 +22,16 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   // printf("do syscall:%d\n",a[0]);
+  Log("do syscall:%d\n",a[0]);
   switch (a[0]) {
     case 0:
-      // printf("a[1]:%d\n",a[1]);
-      // printf("a[2]:%d\n",a[2]);
-      // printf("a[3]:%d\n",a[3]);
       halt(0);
       break;
     case 1:
       yield();
       break;
     case 4:
-      // do write
       write(a);
-      // printf("a[1]:%d\n",a[1]);
-      // printf("a[2]:%x\n",a[2]);
-      // printf("a[3]:%d\n",a[3]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
