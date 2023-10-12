02822afa208e36636fb3d7d575857477d481fcee
tracer-ics2023 <tracer@njuics.org>
2023-10-12 15:05:45 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:05:45 up 12:08,  1 user,  load average: 0.11, 0.09, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,7 @@
 #include <common.h>
 #include "syscall.h"
 void halt(int code);
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -14,6 +15,12 @@ void do_syscall(Context *c) {
       break;
     case 1:
       yield();
+      break;
+    case 4:
+      // do write
+      printf("a[1]:%d\n",a[1]);
+      printf("a[2]:%d\n",a[2]);
+      printf("a[3]:%d\n",a[3]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
