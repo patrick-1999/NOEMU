115a35ecfc8facf4811c59d017bb7c28aa3d2e1c
tracer-ics2023 <tracer@njuics.org>
2023-10-12 14:19:31 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:19:31 up 11:21,  1 user,  load average: 0.44, 0.29, 0.25

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -111,6 +111,7 @@ void assert_fail_msg() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
+  printf("nemu_state.halt_ret:%d\n",nemu_state.halt_ret);
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
