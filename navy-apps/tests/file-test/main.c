376904dd6a03da89b84963a3d5ef28a235e53524
tracer-ics2023 <tracer@njuics.org>
2023-10-19 11:32:12 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:32:12 up 1 day, 23:13,  1 user,  load average: 0.83, 0.94, 1.04

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <assert.h>
 
 int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
   printf("/share/files/num:file opened\n");
+  write(1, "Hello World!\n", 13);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
@@ -41,6 +43,7 @@ int main() {
   fclose(fp);
 
   printf("PASS!!!\n");
+  write(1, "Hello World!\n", 13);
 
   return 0;
 }
