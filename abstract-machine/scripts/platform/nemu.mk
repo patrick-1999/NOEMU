739d876889c9e1d4ce460940fb8214d96d3256d9
tracer-ics2023 <tracer@njuics.org>
2023-10-23 19:20:42 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:20:42 up  6:34,  1 user,  load average: 1.33, 0.41, 0.24

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -30,7 +30,8 @@ run: image
     # $(info $(ISA))
     # $(info $(NEMUFLAGS))
     # $(info $(IMAGE))
-	$(MAKE) -s -C  $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+	$(MAKE) -s -C  $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=/home/rlk/rlk/pxh/ics2023/am-kernels/tests/cpu-tests/tests/test/demo.bin
+    
 
 gdb: image
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
