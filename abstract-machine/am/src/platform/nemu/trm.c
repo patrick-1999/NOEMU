76cb20987442dba26178b2a06addd537fc6e4052
tracer-ics2023 <tracer@njuics.org>
2023-10-24 20:23:14 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:23:14 up 16:01,  1 user,  load average: 0.42, 0.24, 0.15

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -23,7 +23,7 @@ void halt(int code) {
 
 void _trm_init() {
   int ret = main(mainargs);
-  printf("%x\n",heap.start);
-  printf("%x\n",heap.end);
+  printf("heap.start:%x\n",heap.start);
+  printf("heap.end:%x\n",heap.end);
   halt(ret);
 }
