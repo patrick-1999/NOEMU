ebe5e3253c24fb7ade19c48af9eb73754bb754d5
tracer-ics2023 <tracer@njuics.org>
2023-10-26 12:02:54 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:02:54 up 1 day,  4:15,  1 user,  load average: 0.02, 0.13, 0.17

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -42,7 +42,7 @@ void do_syscall(Context *c) {
       c->GPRx = 0; 
       break;
     case SYS_open:
-      yield();
+      // yield();
       c->GPRx = fs_open((void *)a[1], a[2], a[3]);
       break;
     case SYS_read:
