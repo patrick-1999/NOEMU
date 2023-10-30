658e8e1ec092aaf50a445361b8309021d3247816
tracer-ics2023 <tracer@njuics.org>
2023-10-30 18:53:11 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:53:11 up 1 day, 21:37,  1 user,  load average: 0.62, 0.72, 0.65

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -23,7 +23,6 @@ int NDL_PollEvent(char *buf, int len) {
   // printf("evtdev:%d\n",evtdev);
   buf[0]='\0';
   assert(evtdev!=-1);
-  printf("NDL_PollEvent\n");
   return read(evtdev, buf, len);
 }
 
