cb4eb59a383093aea210ce05e1fcccc763d71695
tracer-ics2023 <tracer@njuics.org>
2023-11-01 20:20:20 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:20:20 up 2 days,  8:34,  1 user,  load average: 0.83, 2.07, 1.90

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -101,6 +101,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   gpu_fbdraw.x = offset % width;
   gpu_fbdraw.y = offset / width;
   gpu_fbdraw.sync = 0;
+  printf("pixel:%s\n",gpu_fbdraw.pixels);
   ioe_write(AM_GPU_FBDRAW, &gpu_fbdraw);
 
   return len;
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -81,7 +81,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     lseek(fbdev, x + (y + row) * disp_size.w, SEEK_SET);
     write(fbdev, pixels + row * w, w);
   }
-  write(fbdev, 0, 0);
+  // write(fbdev, 0, 0);
 
 }
 
