804381f074d1563a1b9c0e2512ef522ddab730a9
tracer-ics2023 <tracer@njuics.org>
2023-11-01 20:27:35 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:27:35 up 2 days,  8:41,  1 user,  load average: 0.08, 1.00, 1.54

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -93,6 +93,9 @@ assert(fd >= 0 && fd < sizeof(file_table) / sizeof(Finfo));
   }
 }
 size_t fs_write(int fd, const void *buf, size_t len){
+    if(fd==5){
+      printf("bufL:%s\n",(char *)buf);
+    }
     assert(fd >= 0 && fd < sizeof(file_table) / sizeof(Finfo));
     Finfo *f = &file_table[fd];
     if (fd == FD_STDOUT || fd == FD_STDERR) {
