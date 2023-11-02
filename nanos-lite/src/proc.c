1b8b99575cb352f7c1a44418ddb4ac00f0e0278e
tracer-ics2023 <tracer@njuics.org>
2023-11-02 13:18:20 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:18:20 up 2 days, 13:32,  1 user,  load average: 2.08, 4.20, 3.13

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(&pcb_boot,"/bin/bmp-test");
+  naive_uload(&pcb_boot,"/bin/nslider");
 }
 
 Context* schedule(Context *prev) {
