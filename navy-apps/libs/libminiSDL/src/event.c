cbf7d283d51dda3f95182ccf930feeeff90172a9
tracer-ics2023 <tracer@njuics.org>
2023-11-02 15:08:38 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:08:38 up 2 days, 15:23,  1 user,  load average: 0.40, 1.68, 4.10

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -2,11 +2,14 @@
 #include <SDL.h>
 
 #define keyname(k) #k,
+#define NR_KEYS ( sizeof(keyname)/sizeof(keyname[0]) )
 
 static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
 };
+
+static uint8_t keysnap[NR_KEYS] = {0};
 
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
@@ -17,6 +20,26 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  
+  #define BUF_LEN 16
+  char buf[BUF_LEN];
+  memset(buf,0,BUF_LEN);
+  
+  while(NDL_PollEvent(buf, BUF_LEN) == 0){};
+  if(strncmp(buf, "kd", 2) == 0 ) 
+    event->type = SDL_KEYDOWN;
+  else if(strncmp(buf, "ku", 2) == 0) 
+    event->type = SDL_KEYUP;
+
+  for(size_t i = 0; i < NR_KEYS; ++i){
+    if(    ((strlen(buf + 3) - 1) == strlen(keyname[i]))  \
+        && (strncmp(buf + 3, keyname[i], strlen(keyname[i])) == 0) ) 
+      {event->key.keysym.sym = i; 
+       keysnap[i] = (event->type == SDL_KEYDOWN)? 1: 0;
+       break; 
+      }
+  }
+  
   return 1;
 }
 
