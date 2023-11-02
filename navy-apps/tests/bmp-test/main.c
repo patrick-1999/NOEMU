e9234b63b460b31a378c141f061dd9e2a20dc6c2
tracer-ics2023 <tracer@njuics.org>
2023-11-02 18:41:34 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:41:34 up 2 days, 15:40,  1 user,  load average: 8.05, 4.40, 3.74

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,7 +7,7 @@
 int main() {
   NDL_Init(0);
   int w, h;
-  void *bmp = BMP_Load("/share/pictures/slides-0-300-200.bmp", &w, &h);
+  void *bmp = BMP_Load("/share/pictures/slides-0-400-300.bmp", &w, &h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   NDL_DrawRect(bmp, 0, 0, w, h);
