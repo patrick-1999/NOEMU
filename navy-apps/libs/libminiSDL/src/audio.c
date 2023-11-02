f3c44b9223a8191ea6f510102c7547a81434b1a7
tracer-ics2023 <tracer@njuics.org>
2023-11-02 19:41:16 +0800

    >  compile NEMU 222050208 patrick Linux rlk 5.4.0-26-generic #30-Ubuntu SMP Mon Apr 20 16:58:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:41:16 up 2 days, 16:40,  1 user,  load average: 0.88, 0.92, 2.44

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -3,22 +3,22 @@
 #include <assert.h>
 
 int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained) {
-  assert(0);
+  // assert(0);
   return 0;
 }
 
 void SDL_CloseAudio() {
-  assert(0);
+  // assert(0);
   return 0;
 }
 
 void SDL_PauseAudio(int pause_on) {
-  assert(0);
+  // assert(0);
   return 0;
 }
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
-    assert(0);
+    // assert(0);
   return 0;
 }
 
@@ -27,16 +27,16 @@ SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audi
 }
 
 void SDL_FreeWAV(uint8_t *audio_buf) {
-  assert(0);
+  // assert(0);
   return 0;
 }
 
 void SDL_LockAudio() {
-  assert(0);
+  // assert(0);
   return 0;
 }
 
 void SDL_UnlockAudio() {
-  assert(0);
+  // assert(0);
   return 0;
 }
